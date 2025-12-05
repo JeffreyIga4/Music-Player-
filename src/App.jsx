bd2eaea0615ec3607e144dabdc9abe94ad47f522
Jeffrey Igala <jeffreyigala@MACBOOKPRO.station>
2025-12-05 15:11:30 +0000

    website sections created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import { MusicPlayer } from "./components/MusicPlayer";
 
 function App() {
 
-  return<></>
-  
+  return (
+  <div className="app">
+    {/*<Navbar/> */}
+    <main className="app-main">
+      <div className="player-section"></div>
+      <MusicPlayer />
+      <div className="content-section"></div>
+    </main>
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,4 @@
+
+export const MusicPlayer = () => {
+    return <div></div>
+};
