f5d18a0c6d21f71d5bb37386da339bf0a925c6c9
Jeffrey Igala <jeffreyigala@MACBOOKPRO.station>
2025-12-07 16:00:03 +0000

    fixing bloated git by rewriting history

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { AllSongs } from "./components/AllSongs";
+import { MusicPlayer } from "./components/MusicPlayer";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Playlists } from "./components/Playlists";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className="app">
+        {/*<Navbar/> */}
+        <main className="app-main">
+          <div className="player-section">
+            <MusicPlayer />
+          </div>
+          <div className="content-section">
+            <Routes>
+              <Route path="/" element={<AllSongs />} />
+              <Route path="/playlists" element={<Playlists />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ src/components/Playlists.jsx
@@ -0,0 +1,4 @@
+
+export const Playlists = () => {
+    return <div>Playlists</div>
+};
